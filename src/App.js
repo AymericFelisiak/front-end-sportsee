49dcf8268b5d1b12757947aae238eb3c7e03d316
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-10-18 11:19:27 +0200

    Added mocked data and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './style/sass/style.scss'
-import React from 'React'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { User } from './pages/user/User';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/user/:id" element={<User />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
