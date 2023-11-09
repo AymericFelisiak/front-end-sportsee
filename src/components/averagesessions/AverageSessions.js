2f5315f0b273216bb0bdda4196d4705c007274e7
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-11-09 20:30:46 +0100

    Added title to linegraph

--- src/components/averagesessions/AverageSessions.js
+++ src/components/averagesessions/AverageSessions.js
@@ -4,6 +4,7 @@ import AverageSessionsLineChart from './AverageSessionsLineChart';
 export default function AverageSessions() {
     return (
         <div className="averagesessions-container">
+            <h3>Durée moyenne des <br />sessions </h3>
             <AverageSessionsLineChart />
         </div>
     );
