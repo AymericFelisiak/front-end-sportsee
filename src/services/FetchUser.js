f9cb95f49fce800220662aef76f29a73aeb3ae92
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-11-23 11:30:31 +0100

    Removed unnecesarry async/await

--- src/services/FetchUser.js
+++ src/services/FetchUser.js
@@ -6,12 +6,26 @@ import UserMainDataService from './UserMainDataService';
 // Main call to retrieve user data by its id. This data will be used to create the user object.
 
 export default async function FetchUser(id) {
+    const MAIN_DATAA = UserMainDataService(id);
+    const ACTIVITY_DATAA = UserActivityDataService(id);
+    const AVERAGE_SESSIONSA = UserAverageSessionsDataService(id);
+    const PERFORMANCEA = UserPerformanceDataService(id);
+
+    await Promise.all([
+        MAIN_DATAA,
+        ACTIVITY_DATAA,
+        AVERAGE_SESSIONSA,
+        PERFORMANCEA
+    ]);
+
     const user = {
-        MAIN_DATA: await UserMainDataService(id),
-        ACTIVITY_DATA: await UserActivityDataService(id),
-        AVERAGE_SESSIONS: await UserAverageSessionsDataService(id),
-        PERFORMANCE: await UserPerformanceDataService(id)
+        MAIN_DATA: await MAIN_DATAA,
+        ACTIVITY_DATA: await ACTIVITY_DATAA,
+        AVERAGE_SESSIONS: await AVERAGE_SESSIONSA,
+        PERFORMANCE: await PERFORMANCEA
     };
+
+    console.log(user);
 
     return user;
 }
